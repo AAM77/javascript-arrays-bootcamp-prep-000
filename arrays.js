da2fa62d1decf434438d945d3b643d89439497d9
AAM77 <adeel.a.mhd@gmail.com>
2017-11-16 16:43:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray (array, newElement) {
-  
+  var arrayNew = [newElement, ...array];
 }
 function destructivelyAddElementToBeginningOfArray (array, newElement) {
   array[1] = [newElement, ...array];
